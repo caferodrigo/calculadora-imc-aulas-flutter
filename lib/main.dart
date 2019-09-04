d83e0a6c214ffdba8cf1c688bd9864755cd8582a
caferodrigo <53840583+caferodrigo@users.noreply.github.com>
2019-09-03 21:45:31 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
+import 'package:calculadora_imc/pessoa.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Home(),
-        debugShowCheckedModeBanner: false,
-      ),
-    );
+void main() => runApp(MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+enum SingingCharacter { masculino, feminino }
 
+SingingCharacter _character = SingingCharacter.masculino;
+
+class _HomeState extends State<Home> {
   TextEditingController _weightController = TextEditingController();
   TextEditingController _heightController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   String _result;
+  double _imc;
+  String _generoText;
+
+  Color _corClassificacao;
 
   @override
   void initState() {
@@ -28,111 +34,114 @@ class _HomeState extends State<Home> {
   void resetFields() {
     _weightController.text = '';
     _heightController.text = '';
+    _generoText = '';
     setState(() {
-      _result = 'Informe seus dados';
+      _result = ' Entre com seus dados';
+      _imc = 0;
+      _corClassificacao = Colors.black;
+    });
+  }
+
+  void setPessoa(){
+
+    setState(() {
+
+      if (_character == SingingCharacter.masculino){
+          _generoText = 'masculino';
+      }
+      else{
+          _generoText = 'feminino';
+      }
+
+      Pessoa pessoa = Pessoa(double.parse(_weightController.text), double.parse(_heightController.text), _generoText);
+                                  
+      _imc = pessoa.calcularImc();
+      _result = "IMC = ${_imc.toStringAsPrecision(3)}\n" + pessoa.classificarImc(_imc);
+      _corClassificacao = pessoa.corClassificacao;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: buildAppBar(),
+        appBar: AppBar(
+          title: Text('CALCULADORA IMC'),
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                resetFields();
+              },
+            )
+          ],
+        ),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
-            padding: EdgeInsets.all(20.0), child: buildForm()));
-  }
-
-  AppBar buildAppBar() {
-    return AppBar(
-      title: Text('Calculadora de IMC'),
-      backgroundColor: Colors.blue,
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: () {
-            resetFields();
-          },
+            padding: EdgeInsets.all(20.0),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: 'Peso (kg)'),
+                      controller: _weightController,
+                      validator: (text) {
+                        return text.isEmpty ? "Insira seu peso!" : null;
+                      },
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: 'Altura (cm)'),
+                      controller: _heightController,
+                      validator: (text) {
+                        return text.isEmpty ? "Insira sua altura!" : null;
+                      },
+                    ),
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10.0),
+                        child: Column(
+                           children: <Widget>[
+                           ListTile(
+                              title: const Text('MASCULINO'),
+                              leading: Radio(
+                              value: SingingCharacter.masculino,
+                              groupValue: _character,
+                              onChanged: (SingingCharacter value) {
+                              setState(() { _character = value; });
+                              })),
+                           ListTile(
+                              title: const Text('FEMININO'),
+                              leading: Radio(
+                              value: SingingCharacter.feminino,
+                              groupValue: _character,
+                              onChanged: (SingingCharacter value) {
+                              setState(() { _character = value; });
+                              }))
+                          ])),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: Text(_result, textAlign: TextAlign.center, style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: _corClassificacao)),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 30.0),
+                        child: Container(
+                            height: 50,
+                            child: RaisedButton(
+                              color: Colors.greenAccent,
+                              onPressed: () {
+                                if (_formKey.currentState.validate()) {
+                                  setPessoa();
+                                }
+                              },
+                              child: Text('CALCULAR', style: TextStyle(color: Colors.black)),
+                            )))                  
+                ]
+              )
+          )
         )
-      ],
-    );
-  }
-
-  Form buildForm() {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          buildTextFormField(
-              label: "Peso (kg)",
-              error: "Insira seu peso!",
-              controller: _weightController),
-          buildTextFormField(
-              label: "Altura (cm)",
-              error: "Insira uma altura!",
-              controller: _heightController),
-          buildTextResult(),
-          buildCalculateButton(),
-        ],
-      ),
-    );
-  }
-
-  void calculateImc() {
-    double weight = double.parse(_weightController.text);
-    double height = double.parse(_heightController.text) / 100.0;
-    double imc = weight / (height * height);
-
-    setState(() {
-      _result = "IMC = ${imc.toStringAsPrecision(2)}\n";
-      if (imc < 18.6)
-        _result += "Abaixo do peso";
-      else if (imc < 25.0)
-        _result += "Peso ideal";
-      else if (imc < 30.0)
-        _result += "Levemente acima do peso";
-      else if (imc < 35.0)
-        _result += "Obesidade Grau I";
-      else if (imc < 40.0)
-        _result += "Obesidade Grau II";
-      else
-        _result += "Obesidade Grau IIII";
-    });
-  }
-
-  Widget buildCalculateButton() {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 36.0),
-      child: RaisedButton(
-        onPressed: () {
-          if (_formKey.currentState.validate()) {
-            calculateImc();
-          }
-        },
-        child: Text('CALCULAR', style: TextStyle(color: Colors.white)),
-      ),
-    );
-  }
-
-  Widget buildTextResult() {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 36.0),
-      child: Text(
-        _result,
-        textAlign: TextAlign.center,
-      ),
-    );
-  }
-
-  Widget buildTextFormField(
-      {TextEditingController controller, String error, String label}) {
-    return TextFormField(
-      keyboardType: TextInputType.number,
-      decoration: InputDecoration(labelText: label),
-      controller: controller,
-      validator: (text) {
-        return text.isEmpty ? error : null;
-      },
     );
   }
 }
